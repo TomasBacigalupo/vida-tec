dde1fe2b41a7f04a146436d8b1d53b081f5ceceb
TOMASBACIGALUPO <tbacigalupo@itba.edu.ar>
2020-09-29 10:56:48 -0300

    metodos put bloqueantes con mutex

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,15 @@ import { Extraction } from "./model/extraction";
 import { Account } from "./model/account";
 import { StatusCreated } from "./status/statusCreated";
 import { StatusConflict } from "./status/statusConflict";
+import {Mutex, MutexInterface, Semaphore, SemaphoreInterface, withTimeout} from "async-mutex";
 
 const server = express();
 server.use(bodyParser.json());
 
 const port = 8080; // default port to listen
 let account: Account = null;
+
+const mutex = new Mutex();
 
 
 // define a route handler for the default home page
@@ -52,29 +55,44 @@ server.get("/account/owner/moves", (req, res)=>{
 
 });
 
-server.put("/account/owner/debit",(req, res) =>{
-    if(account === null){
-        res.type('json').status(409).json(new StatusConflict("no hay cuenta creada"));
-    }else if(req.body.value <= 0 ){
-        res.type('json').status(500).json(new StatusBadRequest("monto mayor a 0"));
-    }else{
-        account.debit(req.body.value);
-        res.type('json').status(200).json(new StatusOk("dinero depositado"));
+server.put("/account/owner/debit", async (req, res) =>{
+    // lock
+    const release = await mutex.acquire();
+    try {
+        if(account === null){
+            res.type('json').status(409).json(new StatusConflict("no hay cuenta creada"));
+        }else if(req.body.value <= 0 ){
+            res.type('json').status(500).json(new StatusBadRequest("monto mayor a 0"));
+        }else{
+            account.debit(req.body.value);
+            res.type('json').status(200).json(new StatusOk("dinero depositado"));
+        }
+    } finally {
+        release();
     }
+    // unlock
 });
 
-server.put("/account/owner/extract", (req, res)=>{
-    const value = req.body.value;
-    if(account === null){
-        res.type('json').status(409).json(new StatusConflict("no hay cuenta creada"));
-    }else if(value <= 0){
-        res.type('json').status(500).json(new StatusBadRequest("la extraccion debe ser mayor a 0"));
-    }else if(account.balance < value){
-        res.type('json').status(409).json(new StatusConflict("no hay suficiente dinero"));
-    }else{
-        account.extract(value)
-        res.type('json').status(200).json(new StatusOk("dinero extraido"))
+server.put("/account/owner/extract", async (req, res) => {
+    // lock
+    const release = await mutex.acquire();
+    try {
+        const value = req.body.value;
+        if(account === null){
+            res.type('json').status(409).json(new StatusConflict("no hay cuenta creada"));
+        }else if(value <= 0){
+            res.type('json').status(500).json(new StatusBadRequest("la extraccion debe ser mayor a 0"));
+        }else if(account.balance < value){
+            res.type('json').status(409).json(new StatusConflict("no hay suficiente dinero"));
+        }else{
+            account.extract(value)
+            res.type('json').status(200).json(new StatusOk("dinero extraido"))
+        }
+    } finally {
+        release();
     }
+    // unlock
+
 })
 
 
